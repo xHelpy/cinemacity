51521c0813c6dfbbbad8a946be7fecda400b364f
Marcin <uT_3777@zsp3zamosc>
2024-10-21 13:43:32 +0200

    dodanie plików

---
+++ src/App.js
@@ -0,0 +1,24 @@
+
+import './App.css';
+import Buttons from './buttons'
+function App() {
+  return (
+    <div className="App">
+        <div className="Top">
+
+        </div>
+        <div className="LinkButtons">
+            <Buttons></Buttons>
+        </div>
+        <div className="MainData">
+
+        </div>
+
+        <div className="FilmSelection">
+
+        </div>
+    </div>
+  );
+}
+
+export default App;
